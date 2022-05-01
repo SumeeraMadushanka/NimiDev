ac939fb611c87c772d1618c031b857a273ee26e2
SumeeraMadushanka <sumeeramadushanka104@gmail.com>
2022-05-01 20:50:44 +0530

    import components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import UserDashboard from "./components/User/UserDashboard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={[<NavBar />, <Home />]} />
+          <Route path="/login" element={[<NavBar />, <Login />]} />
+          <Route path="/register" element={[<NavBar />, <Register />]} />
+          <Route
+            path="/user-dashboard/:firstName"
+            element={[<NavBar />, <Register />]}
+          />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
