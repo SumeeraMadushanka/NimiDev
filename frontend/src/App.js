e014284df9f9bacd20339c8bcb3903cc99fef336
SumeeraMadushanka <sumeeramadushanka104@gmail.com>
2022-05-03 20:42:05 +0530

    import components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import AdminDashboard from "./components/Admin/AdminDashboard";
 import UserDashboard from "./components/User/UserDashboard";
 
 const App = () => {
@@ -16,7 +17,12 @@ const App = () => {
           <Route path="/register" element={[<NavBar />, <Register />]} />
           <Route
             path="/user-dashboard/:firstName"
-            element={[<NavBar />, <Register />]}
+            element={[<NavBar />, <UserDashboard />]}
+          />
+
+          <Route
+            path="/admin-dashboard/:firstName"
+            element={[ <AdminDashboard />]}
           />
         </Routes>
       </Router>
