cc88ff04a5ee97b75e648bc3e2767d2277b62dc5
SumeeraMadushanka <sumeeramadushanka104@gmail.com>
2022-05-04 23:29:10 +0530

    implement  code

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { NavLink, useLocation, useParams } from "react-router-dom";
+import { NavLink, useParams, useNavigate } from "react-router-dom";
 import { Popover, Button } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 
 import UserProfile from "../User/UserProfile";
-
 const NavBar = () => {
   let Links = [
     { name: "HOME", link: "/" },
@@ -14,12 +13,22 @@ const NavBar = () => {
     { name: "CONTACT", link: "/contact" },
   ];
 
-  const { firstName } = useParams();
+  const { firstName, lastName, contactNo, type, email, authToken, id } = useParams();
 
   const [open, setOpen] = useState(false);
 
-  const location = useLocation();
-  const { pathname } = location;
+  const history = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("authToken", authToken);
+    localStorage.removeItem("firstName", firstName);
+    localStorage.removeItem("lastName", lastName);
+    localStorage.removeItem("type", type);
+    localStorage.removeItem("contactNo", contactNo);
+    localStorage.removeItem("email", email);
+    localStorage.removeItem("id", id);
+    history("/login");
+  };
 
   const content = (
     <div style={{ width: "2px" }}>
@@ -27,88 +36,110 @@ const NavBar = () => {
         <UserProfile />
       </div>
       <div className="mt-1">
-        <NavLink to="/login">
-          <Button className="w-20">Logout</Button>
-        </NavLink>
+        {/* <NavLink to="/login"> */}
+        <Button className="w-20" onClick={logoutHandler}>
+          Logout
+        </Button>
+        {/* </NavLink> */}
       </div>
     </div>
   );
 
   return (
     <>
-      <div className="shadow-md w-full  top-0 left-0 z-10">
-        <div className="md:flex items-center justify-between bg-zinc-800 py-1 md:px-10 px-7">
-          <div className="font-bold -translate-x-7 text-2xl cursor-pointer flex items-center font-[Poppins] text-sky-600">
-            <div className=" text-5xl translate-y-0.5">
-              <ion-icon name="logo-windows"></ion-icon>
-            </div>
-            <NavLink to="/">
+      {localStorage.getItem("type") === "user" ? (
+        <div className="shadow-md w-full  top-0 left-0 z-10">
+          <div className="md:flex items-center justify-between bg-zinc-800 py-1 md:px-10 px-7">
+            <div className="font-bold -translate-x-7 text-2xl cursor-pointer flex items-center font-[Poppins] text-sky-600">
+              <div className=" text-5xl translate-y-0.5">
+                <ion-icon name="logo-windows"></ion-icon>
+              </div>
               <span class="ml-3 text-xl pl-1 text-sky-600">
                 WinMac Computers
               </span>
-            </NavLink>
-          </div>
-          <div
-            onClick={() => setOpen(!open)}
-            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-          >
-            <ion-icon name={open ? "close" : "menu-sharp"}></ion-icon>
-          </div>
-
-          <ul
-            className={`md:flex md:items-center font-semibold md:pb-0 mt-3  pb-0 absolute md:static bg-zinc-800  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-10 transition-all duration-500 ease-in ${
-              open ? "top-21 opacity-100" : "top-[-490px]"
-            } md:opacity-100`}
-          >
-            {Links.map((Link) => (
-              <li key={Link.name} className="md:ml-2 text-base md:my-0 my-7">
-                <a
-                  href={Link.link}
-                  className=" text-white hover:text-sky-500 hover:bg-gray-700 py-2 hover:py-2 px-4 hover:px-4 hover:rounded-full  duration-500"
-                >
-                  {Link.name}
-                </a>
-              </li>
-            ))}
-            <div className="flex space-x-6">
-              {pathname === "/" ||
-              pathname === "/login" ||
-              pathname === "/register" ||
-              pathname === "/contact" ||
-              pathname === "/services" ||
-              pathname === "/about" ? (
-                <NavLink to="/login">
-                  <button className="inline-flex items-center bg-red-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-black rounded-full text-base mt-4 md:mt-0 translate-x-6">
-                    Login
-                    <ion-icon name="log-in"></ion-icon>
-                  </button>
-                </NavLink>
-              ) : (
-                <div className=" flex ite">
-                  <div className="bg-white w-28 hover:opacity-75 rounded-3xl flex justify-between items-center p-1 cursor-pointer">
-                    <ShoppingCartOutlined className=" text-3xl translate-x-1" />
-                    <div className=" rounded-full text-lg  border-4 -translate-x-2 border-red-400 px-1 text-slate-900 ">
-                      01
-                    </div>
-                  </div>
-                  <button className="inline-flex items-center bg-sky-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-black rounded-full text-base mt-4 md:mt-0 translate-x-6">
-                    <Popover
-                      placement="bottom"
-                      content={content}
-                      title={`Hello ${firstName}`}
-                      trigger="hover"
-                    >
-                      My Account
-                    </Popover>
-                  </button>
-                </div>
-              )}
             </div>
-          </ul>
+            <div
+              onClick={() => setOpen(!open)}
+              className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+            >
+              <ion-icon name={open ? "close" : "menu-sharp"}></ion-icon>
+            </div>
+
+            <ul
+              className={`md:flex md:items-center font-semibold md:pb-0 mt-3  pb-0 absolute md:static bg-zinc-800  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-10 transition-all duration-500 ease-in ${
+                open ? "top-21 opacity-100" : "top-[-490px]"
+              } md:opacity-100`}
+            >
+              <div className=" flex ite">
+                <div className="bg-white w-28 hover:opacity-75 rounded-3xl flex justify-between items-center p-1 cursor-pointer">
+                  <ShoppingCartOutlined className=" text-3xl translate-x-1" />
+                  <div className=" rounded-full text-lg  border-4 -translate-x-2 border-red-400 px-1 text-slate-900 ">
+                  01
+                  </div>
+                </div>
+                <button className="inline-flex items-center bg-sky-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-black rounded-full text-base mt-4 md:mt-0 translate-x-6">
+                  <Popover
+                    placement="bottom"
+                    content={content}
+                    title={`Hello ${firstName}`}
+                    trigger="hover"
+                  >
+                    My Account
+                  </Popover>
+                </button>
+              </div>
+            </ul>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="shadow-md w-full  top-0 left-0 z-10">
+          <div className="md:flex items-center justify-between bg-zinc-800 py-1 md:px-10 px-7">
+            <div className="font-bold -translate-x-7 text-2xl cursor-pointer flex items-center font-[Poppins] text-sky-600">
+              <div className=" text-5xl translate-y-0.5">
+                <ion-icon name="logo-windows"></ion-icon>
+              </div>
+              <NavLink to="/">
+                <span class="ml-3 text-xl pl-1 text-sky-600">
+                  WinMac Computers
+                </span>
+              </NavLink>
+            </div>
+            <div
+              onClick={() => setOpen(!open)}
+              className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+            >
+              <ion-icon name={open ? "close" : "menu-sharp"}></ion-icon>
+            </div>
+
+            <ul
+              className={`md:flex md:items-center font-semibold md:pb-0 mt-3  pb-0 absolute md:static bg-zinc-800  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-10 transition-all duration-500 ease-in ${
+                open ? "top-21 opacity-100" : "top-[-490px]"
+              } md:opacity-100`}
+            >
+              {Links.map((Link) => (
+                <li key={Link.name} className="md:ml-2 text-base md:my-0 my-7">
+                  <a
+                    href={Link.link}
+                    className=" text-white hover:text-sky-500 hover:bg-gray-700 py-2 hover:py-2 px-4 hover:px-4 hover:rounded-full  duration-500"
+                  >
+                    {Link.name}
+                  </a>
+                </li>
+              ))}
+              <NavLink to="/login">
+                <button className="inline-flex items-center bg-red-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-black rounded-full text-base mt-4 md:mt-0 translate-x-6">
+                  Login
+                  <ion-icon name="log-in"></ion-icon>
+                </button>
+              </NavLink>
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 };
+
+
 
 export default NavBar;
